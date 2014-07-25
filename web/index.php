8eb9b49467225990252cbd70c972e6bd9a2e305e
wangqing <zhishan99@163.com>
2014-07-25 16:42:24 +0800

    添加忽略

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@ session_start();
 require_once '..' . DIRECTORY_SEPARATOR . 'includes'.DIRECTORY_SEPARATOR . 'Config.php';
 require_once SERVCIEROOT . 'PageService.php';
 
+
 $page_title="首页";
 //unset($_SESSION['user']);
 //var_dump($_SESSION['user']);exit;
